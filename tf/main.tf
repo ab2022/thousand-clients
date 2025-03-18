8431345ed25189be863a74dc3c8696a710415a48
alex b <alex_balk@comcast.com>
2025-03-18 07:54:40 -0700

    use chromium instead of chrome, update player URL, mv public key to main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -13,15 +13,24 @@ provider "aws" {
     profile = "saml"
 }
 
-data "aws_security_group" "launchwizard2" {
-  id = "sg-026e7af172962f936"
+data "aws_security_group" "ab_subnet" {
+    id = "sg-026e7af172962f936" #update to default subnet
+}
+
+resource "aws_key_pair" "deployer" {
+    key_name   = "deployer-key"
+    public_key = "${file("{/PATH/TO/PUBLIC_SSH_KEY}")}" #add path to public key
 }
 
 resource "aws_instance" "ab_server" {
     count         = 100
+#    ami           = "ami-0acefc55c3a331fa8" #ubuntu for graviton in us-west-2
+#    instance_type = "m8g.xlarge"
     ami           = "ami-00c257e12d6828491"
     instance_type = "c5a.4xlarge"
-    vpc_security_group_ids = [data.aws_security_group.launchwizard2.id]
+    vpc_security_group_ids = [data.aws_security_group.ab_subnet.id]
+    key_name      = "${aws_key_pair.deployer.id}"
+
     user_data     = file("install.sh")
     tags = {
         Name = "Dash Clients ${count.index}"
