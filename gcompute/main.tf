e412e754f3ef387f0c0ce0e557d4c1f5937bb883
alex b <alex_balk@comcast.com>
2025-05-08 09:34:14 -0700

    use multiples zones in gcompute

--- gcompute/main.tf
+++ gcompute/main.tf
@@ -4,12 +4,14 @@ provider "google" {
     credentials = file("PATH/TO/CREDENTIALS/FILE")
 }
 
-resource "google_compute_instance" "default" {
-  count        = 10
-  name         = "vm-${count.index}"
+resource "google_compute_instance" "client" {
+  count        = 15
+  name         = "cli-${count.index}"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  #zone         = "us-central1-b"
   tags         = ["iap"]
+
+  zone         = count.index % 3 == 1 ? "us-central1-a" : count.index % 3 == 2 ? "us-central1-b" : "us-central1-c"
 
   boot_disk {
     initialize_params {
