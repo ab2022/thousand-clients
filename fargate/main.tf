0725421f2bb8bcd365a0835548eeae39fa0dcdfe
alex b <alex_balk@comcast.com>
2025-05-20 08:18:23 -0700

    use updated image and latest URL in fargate

--- fargate/main.tf
+++ fargate/main.tf
@@ -28,7 +28,7 @@ resource "aws_ecs_task_definition" "app_task" {
     [
         {
             "name": "thousands-taskdef",
-            "image": "341487011637.dkr.ecr.us-west-2.amazonaws.com/dashworld:v1.05m",
+            "image": "341487011637.dkr.ecr.us-west-2.amazonaws.com/alpinechrome:new_3",
             "essential": true,
             "memory": 512,
             "cpu": 256,
@@ -39,7 +39,8 @@ resource "aws_ecs_task_definition" "app_task" {
                     "awslogs-group": "/ecs/thousands",
                     "awslogs-stream-prefix": "ecs"
                 }
-            }
+            },
+            "command": [ "https://nightly-dot-shaka-player-demo.appspot.com/demo/#audiolang=en-US;textlang=en-US;cmcd.enabled=true;uilang=en-US;assetBase64=eyJuYW1lIjoic2t5IG5ld3MgIiwic2hvcnROYW1lIjoiIiwiaWNvblVyaSI6IiIsIm1hbmlmZXN0VXJpIjoiaHR0cHM6Ly9zYXRwb2MuY29tOjEyOTEwL2Rhc2gvU2VydmljZTIvbWFuaWZlc3QubXBkIiwic291cmNlIjoiQ3VzdG9tIiwiZm9jdXMiOmZhbHNlLCJkaXNhYmxlZCI6ZmFsc2UsImV4dHJhVGV4dCI6W10sImV4dHJhVGh1bWJuYWlsIjpbXSwiZXh0cmFDaGFwdGVyIjpbXSwiY2VydGlmaWNhdGVVcmkiOm51bGwsImRlc2NyaXB0aW9uIjpudWxsLCJpc0ZlYXR1cmVkIjpmYWxzZSwiZHJtIjpbIk5vIERSTSBwcm90ZWN0aW9uIl0sImZlYXR1cmVzIjpbIlZPRCJdLCJsaWNlbnNlU2VydmVycyI6eyJfX3R5cGVfXyI6Im1hcCJ9LCJvZmZsaW5lTGljZW5zZVNlcnZlcnMiOnsiX190eXBlX18iOiJtYXAifSwibGljZW5zZVJlcXVlc3RIZWFkZXJzIjp7Il9fdHlwZV9fIjoibWFwIn0sInJlcXVlc3RGaWx0ZXIiOm51bGwsInJlc3BvbnNlRmlsdGVyIjpudWxsLCJjbGVhcktleXMiOnsiX190eXBlX18iOiJtYXAifSwiZXh0cmFDb25maWciOm51bGwsImV4dHJhVWlDb25maWciOm51bGwsImFkVGFnVXJpIjpudWxsLCJpbWFWaWRlb0lkIjpudWxsLCJpbWFBc3NldEtleSI6bnVsbCwiaW1hQ29udGVudFNyY0lkIjpudWxsLCJpbWFNYW5pZmVzdFR5cGUiOm51bGwsIm1lZGlhVGFpbG9yVXJsIjpudWxsLCJtZWRpYVRhaWxvckFkc1BhcmFtcyI6bnVsbCwidXNlSU1BIjp0cnVlLCJtaW1lVHlwZSI6bnVsbH0=;panel=CUSTOM%20CONTENT;build=uncompiled" ]
         }
     ]
     DEFINITION
